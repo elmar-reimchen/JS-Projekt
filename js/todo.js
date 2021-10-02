7ae899d4ffe16ad7ea84f2476ab48cc25a24ce2a
elmar-reimchen <lagercrew@gmail.com>
2021-10-03 01:14:11 +0200

    Gried View

--- js/todo.js
+++ js/todo.js
@@ -1,7 +1,7 @@
 //    SELECTORS
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-btn");
-const todoList = document.querySelector(".todo-list");
+const todoList = document.querySelector(".basic-grid");
 
 //    EVENTS
 todoButton.addEventListener("click", addTodo);
@@ -13,29 +13,31 @@ function addTodo(event) {
   event.preventDefault();
 
   // Erstelle div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
+  // const todoDiv = document.createElement("section");
+  // todoDiv.classList.add("basic-grid");
 
   // Erstelle li
-  const newTodo = document.createElement("li");
-  newTodo.innerText = todoInput.value;
-  newTodo.classList.add("todo-item");
-  todoDiv.appendChild(newTodo);
+  const newTodo = document.createElement("div");
+  const newTodoText = document.createElement("h2");
+  newTodoText.innerText = todoInput.value;
+  newTodo.classList.add("card");
+  newTodoText.classList.add("card-text");
 
+  newTodo.appendChild(newTodoText);
   // erstelle Button um Aufgabe als erledigt zu markieren
   const completedButton = document.createElement("button");
   completedButton.innerHTML = "&#10004;";
   completedButton.classList.add("completed-btn");
-  todoDiv.appendChild(completedButton);
+  newTodo.appendChild(completedButton);
 
   // erstelle Button um Aufgabe zu löschen
   const trashButton = document.createElement("button");
   trashButton.classList.add("trash-btn");
   trashButton.classList.add("bx");
   trashButton.classList.add("bx-trash");
-  todoDiv.appendChild(trashButton);
+  newTodo.appendChild(trashButton);
   // div(todoDiv) in ul(todoList) einfügen
-  todoList.appendChild(todoDiv);
+  todoList.appendChild(newTodo);
   // Eingabefeld leeren nachdem aufgabe erstellt wurde
   todoInput.value = "";
 }
@@ -55,5 +57,3 @@ function deleteCheck(e) {
     todo.classList.toggle("completed");
   }
 }
-
- 
